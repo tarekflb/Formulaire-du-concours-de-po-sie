6acb780d714be0730089dffbb714d35fd6f089d0
taraqq <124004765+tarekflb@users.noreply.github.com>
2026-03-26 01:19:06 +0100

    Fix Vite build & Netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',
   server: {
     port: 8080,
     host: '0.0.0.0'
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
